73c6d6f2c984e3b306edc0fc5bc7df831ccaf9ba
Eduardo Rodríguez <eduardo.rodriguez@profuturo.com.mx>
2023-01-24 21:32:43 -0600

    Listas: - Uso del componente LazyColumn - Poblar Column con elementos de una lista

--- app/src/main/java/online/edrh/composable/MainActivity.kt
+++ app/src/main/java/online/edrh/composable/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,28 +26,53 @@ import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.dp
 import online.edrh.composable.ui.theme.ComposableTheme
 
+val template = listOf(
+    MyMessage("Juan", "Hola amigo"),
+    MyMessage("Pedro", "Hola amigo"),
+    MyMessage("Esteban", "Hola amigo"),
+    MyMessage("Itati", "Hola amigo"),
+    MyMessage("Leonardo", "Hola amigo"),
+    MyMessage("Oscar", "Hola amigo"),
+    MyMessage("Pablo", "Hola amigo"),
+    MyMessage("Ximena", "Hola amigo"),
+    MyMessage("Roxana", "Hola amigo"),
+    MyMessage("Liliana", "Hola amigo"),
+    MyMessage("Michael", "Hola amigo"),
+    MyMessage("Alan", "Hola amigo"),
+    MyMessage("Guillermo", "Hola amigo"),
+    MyMessage("Damian", "Hola amigo"),
+    MyMessage("Miguel", "Hola amigo"),
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposableTheme {
-                myComponent()
+            renderUI(template)
+        }
+    }
+}
+
+@Composable
+fun renderUI(messages: List<MyMessage>) {
+    val scrollState = rememberScrollState()
+    ComposableTheme {
+        LazyColumn {
+            items(items = messages) { message ->
+                myComponent(message)
             }
         }
     }
 }
 
 @Composable
-fun myTexts() {
+fun myTexts(message: MyMessage) {
     Column(modifier = Modifier.padding(start = 8.dp)) {
-        myText("Hola jetpack Compose!",
+        myText(message.title,
         MaterialTheme.colorScheme.primary,
         MaterialTheme.typography.titleLarge)
         Spacer(modifier = Modifier.size(10.dp))
-        myText("estas listo?",
-        MaterialTheme.colorScheme.onBackground,
-        MaterialTheme.typography.bodyMedium)
-        myText("Vamos a comenzar",
+        myText(message.body,
         MaterialTheme.colorScheme.onBackground,
         MaterialTheme.typography.bodyMedium)
     }
@@ -67,10 +96,10 @@ fun myImage() {
 }
 
 @Composable
-fun myComponent() {
+fun myComponent(message: MyMessage) {
     Row(modifier = Modifier.padding(top = 8.dp).background(MaterialTheme.colorScheme.background)) {
         myImage()
-        myTexts()
+        myTexts(message)
     }
 }
 
@@ -79,10 +108,7 @@ fun myComponent() {
 showSystemUi = true)
 @Composable
 fun PreviewComponent() {
-    ComposableTheme {
-        Column {
-            myComponent()
-        }
-    }
+    renderUI(template)
 }
 
+data class MyMessage(val title: String, val body: String)
